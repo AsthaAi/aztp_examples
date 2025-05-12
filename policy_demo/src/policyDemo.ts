12f36b7f2d28cf8f75348d8bf7771dadb53e35d5
somitAstha <somit@astha.ai>
2025-05-13 00:00:26 +0545

    Update policy demo with new methods.

--- policy_demo/src/policyDemo.ts
+++ policy_demo/src/policyDemo.ts
@@ -30,33 +30,67 @@ async function main() {
     });
     console.log("AZTP ID:", securedAgent);
 
+    // Verify the identity
+    console.log("2. Verifying identity for agent:", agentName);
+    const verifiedAgent = await aztpClient.verifyIdentity(securedAgent);
+    console.log("Verified Agent:", verifiedAgent);
+
+    // Revoke the identity
+    console.log("3. Revoking identity for agent:", agentName);
+    const revokedAgent = await aztpClient.revokeIdentity(
+      securedAgent.identity.aztpId,
+      "Temporary Revocation"
+    );
+    console.log("Revoked Agent:", revokedAgent);
+
+    // Verify the identity again
+    console.log("4. Verifying identity after revocation for agent:", agentName);
+    const verifiedAgentAfterRevocation = await aztpClient.verifyIdentity(
+      securedAgent
+    );
+    console.log(
+      "Verified Agent after revocation:",
+      verifiedAgentAfterRevocation
+    );
+
+    // Reissue the identity
+    console.log("5. Reissuing identity for agent:", agentName);
+    const reissuedAgent = await aztpClient.reissueIdentity(
+      securedAgent.identity.aztpId
+    );
+    console.log("Reissued Agent after reissue:", reissuedAgent);
+
+    // Verify the reissued identity
+    console.log("6. Verifying reissued identity for agent:", agentName);
+    const verifiedAgentAfterReissue = await aztpClient.verifyIdentity(
+      securedAgent
+    );
+    console.log("Verified Reissued Agent:", verifiedAgentAfterReissue);
+
     // Get and display policy information
-    console.log("\n2. Getting policy information for agent:", agentName);
+    console.log("\n7. Getting policy information for agent:", agentName);
     const identityAccessPolicy = await aztpClient.getPolicy(
       securedAgent.identity.aztpId
     );
 
-    // Display policy information
-    console.log("\nPolicy Information:");
-    for (const policy of identityAccessPolicy.data) {
-      console.log("\nPolicy Statement:", policy.policyStatement);
-      if (policy.policyStatement.Statement[0].Effect === "Allow") {
-        console.log(
-          "Statement Effect:",
-          policy.policyStatement.Statement[0].Effect
-        );
-        console.log(
-          "Statement Actions:",
-          policy.policyStatement.Statement[0].Action
-        );
-        if (policy.policyStatement.Statement[0].Condition) {
-          console.log(
-            "Statement Conditions:",
-            policy.policyStatement.Statement[0].Condition
-          );
-        }
-        console.log("Identity:", policy.identity);
+    const policy = aztpClient.getPolicyValue(
+      identityAccessPolicy.data,
+      "code",
+      "policy:9ca1aadf1964"
+    );
+
+    console.log("\nPolicy:", policy);
+
+    if (policy) {
+      const isAllow = aztpClient.isActionAllowed(policy, "list_users");
+      console.log({ isAllow });
+      if (isAllow) {
+        console.log("Allowed to run the action");
+      } else {
+        console.log("Not allowed to run the action");
       }
+    } else {
+      console.log("Policy not found");
     }
   } catch (error) {
     console.error("Error:", error);
